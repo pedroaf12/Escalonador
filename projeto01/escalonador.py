486499167f25fb5f8106d15b6955ca63177fdc05
pedroaf12 <51967841+pedroaf12@users.noreply.github.com>
2023-09-14 18:08:15 -0300

    Add files via upload

--- projeto01/escalonador.py
+++ projeto01/escalonador.py
@@ -19,6 +19,7 @@ qtd_process = len(t_chegada)
 #---------------FCFS (First-Come, First-Served)----------------
 processo = 0
 t_decorrido = 0
+
 t_espera_medio = 0
 t_espera = []
 
@@ -28,15 +29,22 @@ t__retorno = []
 t__resposta_medio = 0
 t__resposta = []
 
+t_restante_processo = []
 
-
+for item in pico:
+    t_restante_processo.append(item)
 
 while True:
+    
+    if t_decorrido < t_chegada[processo]:
+        t_decorrido += 1
+        continue
+    
     t_espera.insert(processo, t_decorrido - t_chegada[processo])
     t__resposta.insert(processo, t_decorrido - t_chegada[processo])
 
-    while pico[processo] > 0:
-        pico[processo] -= 1
+    while t_restante_processo[processo] > 0:
+        t_restante_processo[processo] -= 1
         t_decorrido += 1
     else:
         t__retorno.insert(processo, t_decorrido - t_chegada[processo])
@@ -56,11 +64,58 @@ while True:
         t__resposta_medio /= qtd_process
         break
 
-print(t_decorrido)
-print(t_espera)
-print(t_espera_medio)
-print(t__retorno)
-print(t__retorno_medio)   
-print(t__resposta)
-print(t__resposta_medio)  
+#print(t_decorrido)
+#print(t_espera)
+#print(t_espera_medio)
+#print(t__retorno)
+#rint(t__retorno_medio)   
+#print(t__resposta)
+#print(t__resposta_medio)  
 #---------------SJF (Shortest Job First)----------------
+t_restante_processo.clear()
+
+t_espera_medio = 0
+t_espera.clear()
+
+t__retorno_medio = 0 
+t__retorno.clear()
+
+t__resposta_medio = 0
+t__resposta.clear()
+
+processo = 0
+t_decorrido = 0
+
+for item in pico:
+    t_restante_processo.append(item)
+    
+fila_prontos = []
+fila_prontos_pico =[]
+
+processo_pronto = 0
+cont = 4
+while cont > 0:
+    
+    
+    while t_chegada[processo_pronto] == t_decorrido:
+        fila_prontos.append(t_chegada[processo_pronto])
+        fila_prontos_pico.append(pico[processo_pronto])
+        processo_pronto += 1
+    
+    cont -= 1
+    proximo_processo = min(fila_prontos_pico)
+    proximo_procsso = fila_prontos_pico.index(proximo_processo)
+    
+    while t_restante_processo[proximo_processo] > 0:
+        t_restante_processo[proximo_processo] -= 1
+        t_decorrido += 1
+    
+else:
+    print(fila_prontos)
+    
+    
+    
+    
+        
+    
+    
