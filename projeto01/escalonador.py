83ee6213b66e07f01f8325bae056d4f4f846bf96
pedroaf12 <51967841+pedroaf12@users.noreply.github.com>
2023-09-19 00:53:21 -0300

    Add files via upload

--- projeto01/escalonador.py
+++ projeto01/escalonador.py
@@ -3,6 +3,7 @@
 t_chegada = []
 pico = []
 qtd_process = 0
+QUANTUM = 2
 
 #------------------------------Abrindo o Arquivo-------------------------------
 arquivo = open("entradaTeste.txt")
@@ -15,6 +16,34 @@ for linha in arquivo:
 arquivo.close()
 qtd_process = len(t_chegada)
 
+def calcula_media(t_espera, t_retorno, t_resposta):
+
+    t_espera_medio = 0
+    t_retorno_medio = 0
+    t_resposta_medio = 0
+    
+    for soma in t_espera:
+        t_espera_medio += soma
+    for soma in t_retorno:
+        t_retorno_medio += soma
+    for soma in t_resposta:
+        t_resposta_medio += soma
+
+    t_espera_medio /= qtd_process
+    t_retorno_medio /= qtd_process
+    t_resposta_medio /= qtd_process
+
+    print("ACABOU")
+    print("retorno")
+    print(t_retorno)
+    print(t_retorno_medio)
+    print("resposta")
+    print(t_resposta)
+    print(t_resposta_medio)
+    print("espera")
+    print(t_espera)
+    print(t_espera_medio)
+        
 
 #---------------------------FCFS (First-Come, First-Served)-----------------------
 processo = 0
@@ -52,35 +81,20 @@ while True:
     
 
     if processo == qtd_process :
-        for soma in t_espera:
-            t_espera_medio += soma
-        for soma in t_retorno:
-            t_retorno_medio += soma
-        for soma in t_resposta:
-            t_resposta_medio += soma
-
-        t_espera_medio /= qtd_process
-        t_retorno_medio /= qtd_process
-        t_resposta_medio /= qtd_process
+        calcula_media(t_retorno=t_retorno, t_resposta=t_resposta, t_espera=t_espera)
         break
 
-#print(t_decorrido)
-#print(t_espera)
-#print(t_espera_medio)
-#print(t__retorno)
-#rint(t__retorno_medio)   
-#print(t__resposta)
-#print(t__resposta_medio)  
+
 #-------------------------SJF (Shortest Job First)---------------------------
 t_restante_processo.clear()
 
-t_espera_medio = 0
+#t_espera_medio = 0
 t_espera.clear()
 
-t_retorno_medio = 0 
+#t_retorno_medio = 0 
 t_retorno.clear()
 
-t_resposta_medio = 0
+#t_resposta_medio = 0
 t_resposta.clear()
 
 processo = 0
@@ -95,6 +109,7 @@ fila_prontos =[]
 processo_pronto = 0
 
 cont = 0  #Contador de quantos processos ja estão na fila de prontos
+          #Posso so usar o processo_pronto
 
 while True:
     
@@ -114,9 +129,9 @@ while True:
     #print(fila_prontos)
     #print(index_processo)
 
-    proximo_process = min(fila_prontos)
-    processo = fila_prontos.index(proximo_process)
-    fila_prontos.remove(proximo_process)
+    proximo_processo = min(fila_prontos)
+    processo = fila_prontos.index(proximo_processo)
+    fila_prontos.remove(proximo_processo)
     processo = index_processo[processo]
 
     t_espera.insert(processo, t_decorrido - t_chegada[processo])
@@ -131,33 +146,112 @@ while True:
         index_processo.remove(processo)
     
     if cont >= qtd_process and len(fila_prontos) == 0:
-        for soma in t_espera:
-            t_espera_medio += soma
-        for soma in t_retorno:
-            t_retorno_medio += soma
-        for soma in t_resposta:
-            t_resposta_medio += soma
-
-        t_espera_medio /= qtd_process
-        t_retorno_medio /= qtd_process
-        t_resposta_medio /= qtd_process
+        calcula_media(t_retorno=t_retorno, t_resposta=t_resposta, t_espera=t_espera)
         break
 
     
     #print(cont)
     #print(processo_pronto)
     
-#print(t_retorno)
-#print(t_resposta)
-#print(t_espera)    
-#print(t_retorno_medio)
-#print(t_resposta_medio)
-#print(t_espera_medio)
+
 
 
  #-------------------------RR2 (Shortest Job First)---------------------------  
+t_restante_processo.clear()
+
+t_espera_medio = 0
+t_espera.clear()
+
+t_retorno_medio = 0 
+t_retorno.clear()
+
+t_resposta_medio = 0
+t_resposta.clear()
+
+processo = 0        #Processo atual 
+t_decorrido = 0
+
+
+for item in pico:
+    t_restante_processo.append(item)
+
+
+index_processo = []
+fila_prontos =[]
+saida_cpu = []
+
+for item in t_chegada:
+    saida_cpu.append(item)
+
+proximo_processo = 0
+processo_pronto = 0
+execucao = 0
+
+cont = 0
+    
+while True:
+    
+    while processo_pronto < qtd_process and t_chegada[processo_pronto] <= t_decorrido:
+        fila_prontos.append(pico[processo_pronto])
+        index_processo.append(processo_pronto)
+        processo_pronto += 1
     
     
+    if len(fila_prontos) == 0:
+        t_decorrido += 1
+        continue  
+
+    
+    processo = index_processo[proximo_processo]
+    
+
+    if fila_prontos[processo] > 0:
+        t_resposta.insert(processo, t_decorrido - t_chegada[processo])
+        fila_prontos[processo] = 0
+        t_espera.insert(processo, 0)
+
+    if t_restante_processo[processo] > 0:
+        t_espera[processo] += t_decorrido - saida_cpu[processo]
+        #print(t_espera)
+
+    while t_restante_processo[processo] > 0 and execucao < QUANTUM:
+        t_restante_processo[processo] -= 1
+        t_decorrido += 1
+        execucao += 1
+        saida_cpu[processo] = t_decorrido
+
+        if t_restante_processo[processo] == 0:
+            t_retorno.insert(processo, t_decorrido - t_chegada[processo])
+            cont += 1
         
+    else:
+        while processo_pronto < qtd_process and t_chegada[processo_pronto] <= t_decorrido:
+            fila_prontos.append(pico[processo_pronto])
+            index_processo.append(processo_pronto)
+            processo_pronto += 1
+
+        aux = index_processo[proximo_processo]
+        index_processo[proximo_processo] = index_processo[len(index_processo) -1]
+        index_processo[len(index_processo) -1] = aux
+
+        execucao = 0
+        
+        if len(fila_prontos) == 1 and t_restante_processo[processo] == 0:
+            t_decorrido += 1
+            continue
+        
+        proximo_processo += 1
+        
+        
+    if proximo_processo == len(index_processo) - 1 or len(fila_prontos) == 1:
+            proximo_processo = 0   
+    
+    if cont == qtd_process:
+        calcula_media(t_retorno=t_retorno, t_resposta=t_resposta, t_espera=t_espera)
+        break
+        
+print(t_resposta_medio)
+
+
     
     
